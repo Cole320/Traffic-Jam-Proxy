aeaff6da13e8324d2a40c5d9ee6c3957149a0bea
Coal <44954117+Cole320@users.noreply.github.com>
2021-10-15 12:08:30 +0000

    test lamba function

---
+++ netlify/functions/hello.js
@@ -0,0 +1,7 @@
+// Simple "Hello World" test
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Hello World"})
+    };
+}
